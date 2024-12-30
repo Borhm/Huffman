445427eed42aac2c6fe36d09b0949e77df83e094
Borhm <sahr0124851310@gmail.com>
2024-12-30 17:32:31 +0200

    yayran

--- Phase1.cpp
+++ Phase1.cpp
@@ -104,6 +104,7 @@ void GenerateCode(cNode* root, string code)
         cout << "Character: " << root->Character << " -> Code: " << root->code << endl;
     }
 
+bolbol7ayran
     // Recur for left and right children
     GenerateCode(root->pleft, code + "0");
     GenerateCode(root->pright, code + "1");
